ff6194daa666b4ddd87644896310f9185a32ee42
asafaya19 <alisafaya@gmail.com>
2020-03-10 18:52:42 +0300

    prep data file

---
+++ data/enwik8/prep_data.jl
@@ -0,0 +1,30 @@
+# download and extract data if data/ directory does not exist
+if !isfile("enwik8")
+    enwik8 = download("http://www.mattmahoney.net/dc/enwik8.zip");
+    run(`unzip $enwik8`)
+    rm(enwik8)
+end
+
+if isfile("train.txt")
+    println("Tokenized enwik8 already exists - skipping processing")
+else
+    f = open("enwik8", "r");
+    data = read(f, String);
+    println("Length of enwik8: ", length(data));
+    num_test_chars = 5000000;
+
+    train_data = data[1:length(data) - (2 * num_test_chars)];
+    valid_data = data[length(train_data) + 1: length(train_data) + num_test_chars];
+    test_data = data[length(train_data) + num_test_chars + 1:end];
+
+    for (fn, part) in [("train.txt", train_data), ("valid.txt", valid_data), ("test.txt", test_data)]
+        println(fn, " will have ", length(part), " bytes")
+        println("- Tokenizing...")
+        part_str = join(part, " ");
+        println("- Writing...")
+        open(fn, "w") do f
+            write(f, part_str)
+        end;
+        # f = open(fn + '.raw', 'wb').write(part)
+    end
+end;
