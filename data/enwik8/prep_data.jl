5bfb08ed722c2943592e54307e05f9c29d52540c
ali safaya <alisafaya@gmail.com>
2020-03-17 16:26:09 +0300

    edit data prep file

--- data/enwik8/prep_data.jl
+++ data/enwik8/prep_data.jl
@@ -9,7 +9,8 @@ if isfile("train.txt")
     println("Tokenized enwik8 already exists - skipping processing")
 else
     f = open("enwik8", "r");
-    data = read(f, String);
+    data = Array{UInt8, 1}()
+    readbytes!(f, data, typemax(Int))
     println("Length of enwik8: ", length(data));
     num_test_chars = 5000000;
 
@@ -19,9 +20,9 @@ else
 
     for (fn, part) in [("train.txt", train_data), ("valid.txt", valid_data), ("test.txt", test_data)]
         println(fn, " will have ", length(part), " bytes")
-        println("- Tokenizing...")
+        println("- Writing ", part, " ...")
+        write(fn * ".raw", part)
         part_str = join(part, " ");
-        println("- Writing...")
         open(fn, "w") do f
             write(f, part_str)
         end;
